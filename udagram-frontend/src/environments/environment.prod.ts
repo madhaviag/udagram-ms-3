98b8ea08b76f69181637dce70b43220b78d8b082
Thomas Frye <fryethomas23@yahoo.com>
2021-10-31 20:50:14 -0400

    updated config files

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://6f8a1-956698336.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
